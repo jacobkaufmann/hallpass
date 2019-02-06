85aaf9142ece4a19ae280e04ee014936c99136fb
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-02-05 20:55:49 -0600

    Add route handling

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -11,5 +11,18 @@ var store = datastore.NewDatastore(nil)
 func Handler() *gin.Engine {
 	m := gin.Default()
 
+	// User routes
+	m.GET("/api/users/:id", serveUser)
+	m.GET("/api/users", serveUsers)
+	m.POST("/api/users", serveCreateUser)
+	m.DELETE("/api/users/:id", serveDeleteUser)
+	m.PUT("/api/users/:id", serveUpdateUser)
+
+	// Pass routes
+	m.GET("/api/passes/:id", servePass)
+	m.GET("/api/passes", servePasses)
+	m.POST("/api/passes", serveCreatePass)
+	m.PUT("/api/passes/:id", serveUpdatePass)
+
 	return m
 }
